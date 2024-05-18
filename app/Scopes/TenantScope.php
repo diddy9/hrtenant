84925575e373d220c3dfc78ffafd61ca80ca277e
Doyino Delima <doyinodelima@gmail.com>
2024-05-18 16:46:21 +0100

    Added tenant scope to query by tenant

---
+++ app/Scopes/TenantScope.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Scopes;
+  
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Hyn\Tenancy\Contracts\Repositories\HostnameRepository;
+use Hyn\Tenancy\Contracts\Repositories\WebsiteRepository;
+use Hyn\Tenancy\Environment;
+use Hyn\Tenancy\Models\Hostname;
+  
+class TenantScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+    	$hostname = app(\Hyn\Tenancy\Environment::class)->hostname();
+    	$tenant_id = $hostname->id;
+        $builder->where('tenant_id', '=', $tenant_id);
+    }
+}
+
+
+?>
